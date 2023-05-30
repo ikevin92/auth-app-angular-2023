3057f6c051eb651ddaafbf646ddd98e68e3dbbec
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2023-05-29 21:22:48 -0500

    post login

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable, inject, signal } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Injectable, computed, inject, signal } from '@angular/core';
+import { Observable, map, of, tap } from 'rxjs';
 import { environment } from 'src/environments/environments';
-import { AuthStatus, User } from '../interfaces';
+import { AuthStatus, LoginResponse, User } from '../interfaces';
 
 @Injectable({
   providedIn: 'root',
@@ -14,9 +14,29 @@ export class AuthService {
   private _currentUser = signal<User | null>(null);
   private _authStatus = signal<AuthStatus>(AuthStatus.checking);
 
+  //! al mundo exterior
+  public currentUser = computed(() => this._currentUser());
+  public authStatus = computed(() => this._authStatus());
+
   constructor() {}
 
   loginService(email: string, password: string): Observable<boolean> {
-    return of(true);
+    const url = `${this.baseUrl}/auth/login`;
+    const body = {
+      email,
+      password,
+    };
+
+    return this.http.post<LoginResponse>(url, body).pipe(
+      tap(({ token, user }) => {
+        console.log({ token, user });
+        this._currentUser.set(user);
+        this._authStatus.set(AuthStatus.authenticated);
+        localStorage.setItem('token', token);
+      }),
+      map(() => true)
+
+      //! errores
+    );
   }
 }
