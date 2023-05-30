7480dffcff4f0da469ab039629cbdda48c2a63a0
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2023-05-30 00:39:53 -0500

    isNotAuthenticated Guard -  public

--- src/app/auth/guards/is-not-authenticated.guard.ts
+++ src/app/auth/guards/is-not-authenticated.guard.ts
@@ -3,13 +3,16 @@ import { CanActivateFn, Router } from '@angular/router';
 import { AuthStatus } from '../interfaces';
 import { AuthService } from '../services/auth.service';
 
-export const isAuthenticatedGuard: CanActivateFn = (route, state) => {
+// PublicGuard -  PrivateGuard
+
+export const isNotAuthenticatedGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
 
-  if (authService.authStatus() === AuthStatus.authenticated) return true;
-  // if (authService.authStatus() === AuthStatus.checking) return false;
+  if (authService.authStatus() === AuthStatus.authenticated) {
+    router.navigateByUrl('/dashboard');
+    return false;
+  }
 
-  router.navigateByUrl('/auth/login');
-  return false;
+  return true;
 };
