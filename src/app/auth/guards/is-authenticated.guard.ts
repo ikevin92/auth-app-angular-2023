39da8a15e3f9ee2e42e17a6efdfbc1567c5c459e
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2023-05-30 00:17:53 -0500

    remove console.log

--- src/app/auth/guards/is-authenticated.guard.ts
+++ src/app/auth/guards/is-authenticated.guard.ts
@@ -4,18 +4,11 @@ import { AuthStatus } from '../interfaces';
 import { AuthService } from '../services/auth.service';
 
 export const isAuthenticatedGuard: CanActivateFn = (route, state) => {
-  // console.log('isAuthenticatedGuard');
-  // console.log({ route, state });
-
   const authService = inject(AuthService);
   const router = inject(Router);
-  // console.log({ status: authService.authStatus() });
 
   if (authService.authStatus() === AuthStatus.authenticated) return true;
 
-  // const url = state.url;
-  // console.log({ url });
-  // localStorage.setItem('url', url);
   router.navigateByUrl('/auth/login');
   return false;
 };
