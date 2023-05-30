6709b8ae0727ced5760cb6ac1148b7c31107f10b
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2023-05-29 21:01:44 -0500

    inicio del Auth Service

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,9 +1,21 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject, signal } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { environment } from 'src/environments/environments';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  private readonly baseUrl: string = environment.baseUrl;
+  private http = inject(HttpClient);
 
-  constructor() { }
+  private _currentUser = signal<User | null>(null);
+  private _authStatus = signal<AuthStatus>();
+
+  constructor() {}
+
+  loginService({ email: string, password: string }): Observable<boolean> {
+    return of(true);
+  }
 }
